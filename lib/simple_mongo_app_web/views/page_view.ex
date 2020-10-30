54757cd981d70f8b5d6042f43b86417f5561df79
Rod McLaughlin <rodmclaughlin@gmail.com>
2020-10-30 19:36:34 +0300

    [master]:  Got it talking to MongoDB and reporting the number of rows in the database on the home page. It's a start.

--- lib/simple_mongo_app_web/views/page_view.ex
+++ lib/simple_mongo_app_web/views/page_view.ex
@@ -1,3 +1,19 @@
 defmodule SimpleMongoAppWeb.PageView do
   use SimpleMongoAppWeb, :view
+
+  def article_count do
+    Mongo.start_link(
+      name: :mongo,
+      database: "my_app_db",
+      hostname: "localhost",
+      username: "root",
+      password: "rootpassword"
+    )
+    tuple = Mongo.count(:mongo, "my_app_db", %{})
+    if elem( tuple, 0 ) == :ok do
+      "#{ elem( tuple, 1 ) }"
+    else
+      "Error: #{ elem( tuple, 0 ) }"
+    end 
+  end
 end
